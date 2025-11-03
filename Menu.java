20854d585bebe85b43737690f2ef0017c3ad17ac
kireoda <anastasijabernsteine@gmail.com>
2025-11-03 12:35:31 +0000

    added comments and started menu

---
+++ Menu.java
@@ -0,0 +1,26 @@
+java.util.Scanner;
+
+class Menu {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to the Hotel Booking System. Please select a Country:");
+        int selected = scanner.nextInt();
+//Menu of countries.
+
+//Menu of sorting options for hotels (e.g., sort by rating, price, or favourites).
+
+//menu with each hotel’s details:
+//Number of beds, Price per day, Whether it’s already booked or not.
+
+//The user enters a start date and end date.
+
+//Calculates the total cost based on the number of days and the daily rate.
+
+//Display whether the hotel is booked and the total cost.
+
+//Ask the user to confirm the booking (“Are you sure you want to book?”).
+
+//Once confirmed, display a summary of all booking details.
+
+}
+}
